b7552355291eef6637928773f884a59b7e3aa730
Anton Popov <ademaro@ya.ru>
2024-05-11 22:22:09 +0530

    Обновление скриптов

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,12 +1,5 @@
-
 const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-
-if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  themeToggleLightIcon.classList.remove('hidden');
-} else {
-  themeToggleDarkIcon.classList.remove('hidden');
-}
 
 const themeToggleBtn = document.getElementById('theme-toggle');
 
